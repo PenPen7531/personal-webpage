b3cc4a4f78638079d68605c5305ec9e3fa114fa4
Jeff7531 <jeffmkwang@gmail.com>
2022-06-20 00:30:42 -0700

    Added new template

---
+++ static/js/js.js
@@ -0,0 +1,8 @@
+function clickme(){
+    let div_box=document.querySelector(".secret");
+    let image=document.createElement("img");
+    image.src="static/img/cat-banana.jpg";
+    div_box.appendChild(image);
+}
+
+ 
