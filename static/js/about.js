bdf1ea5aca80fd3573dc8e0cdb8a31ec22eb4ee7
Jeff7531 <jeffmkwang@gmail.com>
2022-07-06 00:10:38 -0700

    new css

--- static/js/about.js
+++ static/js/about.js
@@ -2,12 +2,15 @@
 function myFunction(){
     let button=document.getElementById("button");
     let text=document.getElementById("collapse");
+    let list=document.querySelector(".inner-grid");
     if (text.style.display === "inline"){
         text.style.display="none";
+        list.style.display="flex"
         button.innerHTML="View More";
     }
     else{
         text.style.display="inline";
         button.innerHTML="View Less"
+        list.style.display="none"
     }
 }
